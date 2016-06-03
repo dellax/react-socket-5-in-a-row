491b5f489033771df57dd42230191944ec3575a6
unknown <michalnebbia@gmail.com>
2016-06-03 16:28:40 +0200

    fixed indentation

--- app/Square.js
+++ app/Square.js
@@ -2,29 +2,29 @@ import React from 'react';
 import Styles from './App.css';
 
 export const Square = (props) => {
-	let item = '';
-	if (props.data === 'x') {
-		item = <Cross />;
-	} else if (props.data === 'o') {
-		item = <Circle />;
-	}
-	return (
-		<div className={Styles.square}>{item}</div>
-	)
+  let item = '';
+  if (props.data === 'x') {
+    item = <Cross />;
+  } else if (props.data === 'o') {
+    item = <Circle />;
+  }
+  return (
+    <div className={Styles.square}>{item}</div>
+  )
 }
 
 const Circle = () => {
-	const style = {
-		fontSize: '20px',
-		fontWeight: 'bold',
-		color: 'red'
-	}
-	return <div style={style}>&#11096;</div>;
+  const style = {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    color: 'red'
+  }
+  return <div style={style}>&#11096;</div>;
 }
 
 const Cross = () => {
-	const style = {
-		color: 'blue'
-	}
-	return <div style={style}>&#10060;</div>;
+  const style = {
+    color: 'blue'
+  }
+  return <div style={style}>&#10060;</div>;
 }
